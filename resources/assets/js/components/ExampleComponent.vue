17d45297d9e8aa0a7036befcf3e72c0fd54eb105
MIsmail80 <mismail80@gmail.com>
2018-04-15 13:29:33 +0400

    Add new content to example component

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -6,7 +6,15 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        Hello from Example Component!!!
+                        <h1>Hello from Example Component!!!</h1>
+                        <p>
+                            Feature branches (or sometimes called topic branches) are used to develop new features for
+                            the upcoming or a distant future release. When starting development of a feature, the target
+                            release in which this feature will be incorporated may well be unknown at that point. The
+                            essence of a feature branch is that it exists as long as the feature is in development, but
+                            will eventually be merged back into develop (to definitely add the new feature to the
+                            upcoming release) or discarded (in case of a disappointing experiment).
+                        </p>
                     </div>
                 </div>
             </div>
